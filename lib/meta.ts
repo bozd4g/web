5387dcf33c30a379fba5f6cbcce2e2cce6239a13
bozd4g <bozd4g@gmail.com>
2022-10-16 12:40:38 +0200

    Add meta

---
+++ lib/meta.ts
@@ -0,0 +1,16 @@
+const MetaConfig = {
+  title: "Furkan Bozdag",
+  description: "Software Engineer, Bibliophile and Designer",
+  url: "https://bozdag.dev",
+  analytics: "UA-127177068-2",
+  social: {
+    email: "me@furkanbozdag.com",
+    instagram: "https://instagram.com/bozd4g",
+    twitter: "https://twitter.com/bozd4g",
+    medium: "https://medium.com/@bozd4g",
+    github: "https://github.com/bozd4g",
+    superpeer: "https://superpeer.com/bozd4g",
+  },
+};
+
+export default MetaConfig;
