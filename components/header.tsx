d8f8488e7a305ef67bef5cbd9a0cc8de9509ef54
bozd4g <bozd4g@gmail.com>
2022-10-22 09:57:20 +0200

    Complete header

--- components/header.tsx
+++ components/header.tsx
@@ -12,18 +12,26 @@ const Header = () => {
   return (
     <header>
       <Container>
-        <nav className="flex-col gap-3 sm:!flex sm:flex-row">
-          {Pages.filter((e) => e.show).map((page: PageConfig) => {
-            const isActive = page.path === `/${currentPath}`;
-            return (
-              <span key={page.path}>
-                <NextLink href={page.path}>
-                  <a className={clx(isActive ? "shine" : "")}>{page.text}</a>
-                </NextLink>
-              </span>
-            );
-          })}
-        </nav>
+        <div className="mx-auto max-w-2xl lg:max-w-5xl">
+          <div className="flex flex-1 justify-end md:justify-center">
+            <nav className="pointer-events-auto hidden md:block">
+              <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
+                {Pages.filter((e) => e.show).map((page: PageConfig) => {
+                  const isActive = page.path === `/${currentPath}`;
+                  return (
+                    <li key={page.path}>
+                      <NextLink href={page.path}>
+                        <a className={clx("relative block px-3 py-2 transition hover:text-black", !isActive ? "text-gray-500" : "text-black")}>
+                          {page.text}
+                        </a>
+                      </NextLink>
+                    </li>
+                  );
+                })}
+              </ul>
+            </nav>
+          </div>
+        </div>
       </Container>
     </header>
   );
