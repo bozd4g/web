5ced19bd16fec939e0d6690df684260de5d6a436
bozd4g <bozd4g@gmail.com>
2022-10-22 18:21:18 +0200

    Update home page for intoduction info

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
+import Content from "../components/content";
 import Layout from "../components/layout";
+import Text from "../components/typography";
 
 const Index = () => {
-  return <Layout>Index</Layout>;
+  return (
+    <Layout>
+      <Content>
+        <Text
+          variant="h1"
+          className="pt-20 tracking-normal text-zinc-800 max-w-2xl"
+        >
+          Software Engineer, Bibliophile and Designer.
+        </Text>
+        <Text variant="p" className="mt-6 text-base text-zinc-600">
+          I am Furkan, a software engineer based in Berlin, Germany and Istanbul, Turkey.
+          I am writing some blog posts about software engineering and other topics. Also I am creating some open source projects to share my knowledge with the community.
+        </Text>
+      </Content>
+    </Layout>
+  );
 };
 
 export default Index;
