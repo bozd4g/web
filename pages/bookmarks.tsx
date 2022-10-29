43aaff096095cd3c3bcfe78b54e37fdc1aa1e7bd
bozd4g <bozd4g@gmail.com>
2022-10-29 11:34:38 +0200

    Implement card into bookmarks page

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
+import Bookmark from "../components/bookmark";
 import Content from "../components/content";
 import Layout from "../components/layout";
 import Text from "../components/typography";
+import { BookmarkDto } from "../lib/raindrop";
 
 const BookmarksPage = () => {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    const fetchBookmarks = async () => {
+      const response = await fetch("/api/bookmarks");
+      const data = await response.json();
+      setBookmarks(data || []);
+    };
+
+    fetchBookmarks().catch(() => {
+      console.error("Failed to fetch bookmarks");
+      setBookmarks([]);
+    });
+  }, []);
+
   return (
-    <Layout title="bookmars">
+    <Layout title="bookmarks">
       <Content>
         <Text
           variant="h1"
@@ -12,6 +30,23 @@ const BookmarksPage = () => {
         >
           What I liked, might you will too.
         </Text>
+        <Text
+          variant="p"
+          className="mt-6 mb-20 text-base text-zinc-600 max-w-2xl"
+        >
+          I checking my flow every day and saving bookmarks what I liked. You
+          can see and be inspired too.
+        </Text>
+
+        {bookmarks.map((bookmark: BookmarkDto) => (
+          <Bookmark
+            key={bookmark._id}
+            title={bookmark.title}
+            description={bookmark.excerpt}
+            date={new Date(bookmark.created)}
+            link={bookmark.link}
+          />
+        ))}
       </Content>
     </Layout>
   );
