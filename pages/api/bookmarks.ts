bdf6b44f591a1da47e5c737952658561420671fc
bozd4g <bozd4g@gmail.com>
2022-10-29 11:33:56 +0200

    Add raindrop api

--- pages/api/bookmarks.ts
+++ pages/api/bookmarks.ts
@@ -1,8 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import Raindrop, { BookmarkDto } from "../../lib/raindrop";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
-   const { query: { }, method, } = req;
-   return res.status(200).send({ message: 'Hello World' });
+const raindrop = new Raindrop();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+   const { query: { page } } = req;
+   
+   const response = await raindrop.getBookmars(parseInt(page as string));
+   const itemsDto = response.items.map((item: BookmarkDto) => {
+      return {
+         _id: item._id,
+         title: item.title,
+         excerpt: item.excerpt,
+         link: item.link,
+         created: item.created
+      }
+   })
+   return res.status(200).send(itemsDto);
 };
 
 export default handler;
