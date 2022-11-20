857b83efdc0aef802b4cfa75abb1368768aa0f77
bozd4g <bozd4g@gmail.com>
2022-11-20 17:26:05 +0100

    Remove bookmarks page

--- lib/page-config.ts
+++ lib/page-config.ts
@@ -7,7 +7,6 @@ interface PageConfig {
 const Pages: PageConfig[] = [
   { text: "home", path: "/", show: true },
   { text: "about", path: "/about", show: true },
-  { text: "bookmarks", path: "/bookmarks", show: true },
   { text: "posts", path: "/posts", show: true },
   { text: "projects", path: "/projects", show: true },
   { text: "post", path: "/post", show: false },
