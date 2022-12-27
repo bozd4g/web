2d511365d88115e21732e176ded8321790e3838d
bozd4g <bozd4g@gmail.com>
2022-12-27 20:44:16 +0100

    Init next 13

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <main className="text-3xl font-bold underline text-red-400">
+            Hello world
+        </main>
+    )
+}
+
+export default HomePage;
