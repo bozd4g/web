05142596863771d4d4abd4149aaceaaf4e983438
bozd4g <bozd4g@gmail.com>
2023-02-09 07:28:47 +0100

    Add prettier and update index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
-import React from "react";
 import Social from "../components/social";
 
 const HomePage = () => {
-    return (
-        <main className="w-screen h-screen">
-            <div className="h-full flex flex-col justify-center items-center">
-                <p className="font-serif text-zinc-700">it's furkan.</p>
-                <Social className="mt-8"/>
-            </div>
-        </main>
-    )
-}
+	return (
+		<main className="w-screen h-screen">
+			<div className="h-full flex flex-col justify-center items-center">
+				<Social className="mt-8" />
+			</div>
+		</main>
+	);
+};
 
 export default HomePage;
