2e05aa556744c9b468024fada057c82e308de3e2
bozd4g <me@furkanbozdag.com>
2023-04-09 02:12:24 +0200

    Update tailwind globals

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    content: [
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Roboto, sans-serif', ...defaultTheme.fontFamily.sans],
-                serif: ['Open Sans, sans-serif', ...defaultTheme.fontFamily.serif],
-            }
-        }
-    },
-    plugins: [],
-}
+	content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["Montserrat, sans-serif", ...defaultTheme.fontFamily.sans],
+				serif: ["Roboto, sans-serif", ...defaultTheme.fontFamily.serif],
+			},
+		},
+	},
+	plugins: [],
+};
