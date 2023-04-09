40e25994268480dd2f3abc5a8c9e3c3361d49750
bozd4g <me@furkanbozdag.com>
2023-04-09 02:14:22 +0200

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
+import About from "../components/about";
+import Hello from "../components/hello";
 import Noise from "../components/noise";
 import Social from "../components/social";
 
 const HomePage = () => {
 	return (
-		<main className="w-screen h-screen">
+		<div className="h-screen bg-gray-800">
 			<Noise />
-		</main>
+			<div className="absolute top-0 left-0 h-full w-full">
+				<div className="h-screen flex flex-col justify-center items-center">
+					<div>
+						<Hello />
+						<About className="mt-7" />
+						<Social className="mt-7 opacity-70" fill="white" />
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 };
 
