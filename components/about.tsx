8ff197f26ad645d422a44bb9d4cbe0f0b6f3f23c
bozd4g <me@furkanbozdag.com>
2023-04-09 02:16:28 +0200

    Update about page

--- components/about.tsx
+++ components/about.tsx
@@ -7,7 +7,7 @@ interface Props {
 const About = ({className}: Props) => {
 	return (
 		<div className={clx("max-w-lg text-white opacity-70", className)}>
-			<p>I am an engineer who is trying to touch people's life.</p>
+			<p>I am an engineer who is trying to touch people&apos;s life.</p>
 			<p className="mt">
 				Explore is an exciting thing. Travels, music, designs, technologies, development, and writing about something are my area of
 				interests. I like meeting and making new friends!
