d8815bd08b6366b19e403d235720157532e3fb73
bozd4g <me@furkanbozdag.com>
2023-07-24 21:52:42 +0200

    Update page and about

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import About from "../components/about";
-import Hello from "../components/hello";
-import Noise from "../components/noise";
 import Social from "../components/social";
 
 const HomePage = () => {
 	return (
 		<div className="h-screen w-screen bg-[#151515]">
-			<Noise />
 			<div className="absolute top-0 left-0 h-full w-full">
 				<div className="h-screen flex flex-col justify-center items-center">
-					<Hello />
 					<div>
-						<About className="mt-7" />
-						<Social className="mt-7 opacity-70" fill="white" />
+						<About />
+						<Social className="mt-1 opacity-70" fill="white" />
 					</div>
 				</div>
 			</div>
